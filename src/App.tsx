8a4c37a6807fa2cb7e4cd26482fd041c01f42d1b
bishoy.elmalah <bishoy.elmalah@gmail.com>
2026-03-02 19:21:00 +0200

    feat: Implement Find Match Page layout with header, sidebars, and matchmaking hub

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router' 
-import './App.css'
 import { HomePage } from './pages/HomePage/HomePage'
 import { FindMatchPage } from './pages/FindMatchPage/FindMatchPage'
 import { MatchPage } from './pages/MatchPage/MatchPage'
