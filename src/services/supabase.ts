c0cda1191d5c0229a93cda620584c43d6a82dd01
Bavly Peter Barsoum <bavlypeterb@gmail.com>
2026-03-09 00:23:55 +0200

    ady elsaf7ten ely mday2nkm tany

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,5 +1,8 @@
 import {createClient} from '@supabase/supabase-js'
 
-export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+export const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL, 
+    import.meta.env.VITE_SUPABASE_KEY
+)
 
 console.log(supabase.from("users").select());
