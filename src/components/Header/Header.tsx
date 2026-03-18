a72be81cfa84eab07bdbec94cd95a4c6544d4f95
Paula Sameh <paulasameh549@gmail.com>
2026-03-18 14:30:28 +0200

    Last commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,5 @@
-import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { logout } from '../../services/authService';
-import { AuthContext } from '../../contexts/AuthContext';
 import styles from './Header.module.css';
 import { useAuth } from '../../services/authService';
 
