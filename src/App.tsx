2edf1d366ea66ba12463e8e1f44f69b7a18892a4
bishoy.elmalah <bishoy.elmalah@gmail.com>
2026-03-20 23:50:17 +0200

    added UnderConstructionPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
-import { Routes, Route } from 'react-router'
 import './App.css'
-import { LandingPage } from './pages/LandingPage/LandingPage'
-import { HomePage } from './pages/HomePage/HomePage'
-import { FindMatchPage } from './pages/FindMatchPage/FindMatchPage'
-import { MatchPage } from './pages/MatchPage/MatchPage'
-import { VictoryPage } from './pages/VictoryPage/VictoryPage'
-import { LosePage } from './pages/LosePage/LosePage'
-import { LoginPage } from './pages/loginPage/loginPage'
-import {RegisterPage} from './pages/RegisterPage/RegisterPage'
-import { GetReadyPage } from './pages/GetReadyPage/GetReadyPage'
+import { UnderConstructionPage } from './pages/UnderConstructionPage/UnderConstructionPage'
 
 function App() {
-  return (
-    <Routes>
-      <Route index element={<LandingPage />}></Route>
-      <Route path='/home' element={<HomePage/>} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
-      <Route path='/findMatch' element={<FindMatchPage />}></Route>
-      <Route path='/getReady' element={<GetReadyPage />}></Route>
-      <Route path='/match' element={<MatchPage />}></Route>
-      <Route path='/victory' element={<VictoryPage />}></Route>
-      <Route path='/lose' element={<LosePage />}></Route>
-    </Routes>
-  )
+  return <UnderConstructionPage />
 }
 
 export default App
